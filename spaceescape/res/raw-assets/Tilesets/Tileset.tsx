476cdcf0d3cc7c1c65d187c6c3689b809a0a7793
felipesflisboa <felipesflisboa@users.noreply.github.com>
2018-08-12 18:52:54 -0300

    Added Space Escape test

---
+++ spaceescape/res/raw-assets/Tilesets/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset" tilewidth="16" tileheight="16" tilecount="32" columns="8" backgroundcolor="#00ffffff">
+ <image source="Tileset.png" width="128" height="64"/>
+</tileset>
